0673aa357ef51767ce015641499b6d968c17d83f
einav.leyboshor <einav.leyboshor@cybereason.com>
2024-05-27 10:45:29 +0300

    add viewport to pages

--- app/app.py
+++ app/app.py
@@ -34,4 +34,4 @@ def success():
     pass
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5001))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
